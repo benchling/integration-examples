b52ef80c4931e221612816f7c10da922933cd134
Namitha <namitha@benchling.com>
2021-03-10 16:55:54 -0800

    Create lambda_function.py
    Associated code for the Events example for docs.benchling.com: https://docs.benchling.com/docs/example-send-benchling-request-to-a-slack-channel

---
+++ events_examples/benchling_request_to_slack/lambda_function.py
@@ -0,0 +1,60 @@
+# lambda_function.py
+
+import json
+import os
+import urllib3
+
+
+def lambda_handler(event, context):
+    http = urllib3.PoolManager()
+    assignee_handles = []
+    for assignee_handles in event["detail"]["request"]["assignees"]:
+        assignee_handles.append("@" + str(assignee_handles["user"]["handle"]))
+
+    # Create data payload for Slack POST request
+    # see https://api.slack.com/block-kit for more formatting options for the message
+    data = {
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": " New Benchling Request Here!\n"
+                    + "<"
+                    + event["detail"]["request"]["webURL"]
+                    + "|Go To Benchling Request >",
+                },
+            },
+            {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": " *Requestor*:\n"
+                        + "@"
+                        + str(event["detail"]["request"]["creator"]["handle"]),
+                    },
+                    {"type": "mrkdwn", "text": " *Assignee(s)*:\n" + ", ".join(assignee_handles)},
+                    {
+                        "type": "mrkdwn",
+                        "text": " *Comments*:\n"
+                        + str(event["detail"]["request"]["fields"]["request_comments"]["value"]),
+                    },
+                ],
+            },
+            {"type": "divider"},
+        ]
+    }
+
+    # Send POST request to webhook URL generated in Slack App admin settings
+    resp = http.request(
+        "POST",
+        os.environ["SLACK_WEBHOOK_URL"],
+        body=json.dumps(data),
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": "Bearer {}".format(os.environ["SLACK_TOKEN"]),
+        },
+    )
+
+    return {"statusCode": resp.status, "statusData": resp.data}
